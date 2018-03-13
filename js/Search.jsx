9c730cbff6d77eb6146bf78c42cf2af813e4652f
drdlee <danielrudi.lee@gmail.com>
2018-03-13 18:04:54 +0800

    Routing, props, state management, filtering = done

---
+++ js/Search.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import preload from '../data.json';
+import ShowCard from './ShowCard';
+
+class Search extends Component {
+    state = {
+        searchTerm: ''
+    };
+    handleSearchTermChange = event => {
+        this.setState({ searchTerm: event.target.value });
+    };
+    render() {
+        return (
+            <div className="search">
+                <header>
+                    <h1>svideo</h1>
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        value={this.state.searchTerm}
+                        onChange={this.handleSearchTermChange}
+                    />
+                </header>
+                <div>
+                    {preload.shows
+                        .filter(
+                            show =>
+                                `${show.title} ${show.description}`
+                                    .toUpperCase()
+                                    .indexOf(this.state.searchTerm.toUpperCase()) >= 0
+                        )
+                        .map(show => <ShowCard key={show.imdbID} {...show} />)}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Search;
